11c32229856f062ac0457a7bdbfb4f99b0b9e3b7
Sayooj Balakrishnan <bsayooj2012@gmail.com>
2018-10-10 16:57:10 +0530

    Update regression.py

--- Regression_model_script/regression.py
+++ Regression_model_script/regression.py
@@ -1,12 +1,5 @@
-#!usr/bin/python
-import webbrowser
-#taking search input from user
-search_input=raw_input(what do you want to search: ")
-print "Plz Wait youtube is  about to open!!!!"
-#adding two strings
-z="https://www.youtube.com/results?search_query=",search_input
-webbrowser.open_new_tab(z)
- import pandas as pd 
+
+import pandas as pd 
 import numpy as np 
 import matplotlib.pyplot as plt 
 import math
